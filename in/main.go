aa3401b9c112cf39592ac3e0a736994a4069f88d
Garima Sharma <gasharma@pivotal.io>
2020-05-04 22:55:22 -0400

    add nothing to in and check scripts

--- in/main.go
+++ in/main.go
@@ -1,3 +1,22 @@
 package in
 
-func main() {}
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("usage: %s <sources directory>", os.Args[0])
+		os.Exit(1)
+	}
+
+	err := json.NewDecoder(os.Stdin)
+	if err != nil {
+		log.Fatalf("failed to read request: %s", err)
+	}
+	inPutResponse := fmt.Sprintf(`{"inImplemented":"no"}`)
+	fmt.Println(string(inPutResponse))
+}
